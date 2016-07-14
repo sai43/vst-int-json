a8a12e5908e6bbdce1788650a18db6e03dbdfef1
saich <chsai.btech@gmail.com>
2016-07-14 18:28:30 +0000

    associations added between item and category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
+    
+    belongs_to :category
+    
     validates :title, :description, :price, :status, :published_date, presence: true
     
     validates :status, inclusion: { in: %w(available pending sold expired banned),
