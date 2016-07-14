48ada862655f30f79d6b6a86ebc65901aebd461d
saich <chsai.btech@gmail.com>
2016-07-14 18:26:52 +0000

    category mvc and item validations added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ActiveRecord::Base
+    validates :title, :description, :price, :status, :published_date, presence: true
+    
+    validates :status, inclusion: { in: %w(available pending sold expired banned),
+                     message: "%{value} is not a valid status" }
+    
 end
